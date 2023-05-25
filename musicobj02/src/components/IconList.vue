35075e58cb02a0fe9502963c65966a869ebbb2f6
yc <1158462616@qq.com>
2023-05-25 18:10:46 +0800

    “23-5-25”

---
+++ musicobj02/src/components/IconList.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        我是iconlist
+    </div>
+</template>
+
+<script>
+
+export default{
+    name:'iconlist',
+}
+</script>
---
+++ musicobj02/src/components/MusicList.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        我是musiclist
+    </div>
+</template>
+
+<script>
+
+export default{
+    name:'musiclist',
+}
+</script>
